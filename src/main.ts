916834f24fb06f681bdecd5aaf31e27feb5b6e38
tronicboy1 <a.inoue@churadata.okinawa>
2022-01-17 12:15:50 +0900

    remove files and update package.json/tsconfig.json

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,62 @@
-import './style.css'
+const makeTimeoutPromise = (timeoutLength: number) => {
+  return new Promise((_, reject) => {
+    setTimeout(() => {
+      reject(Error("Fetch timeout"));
+    }, timeoutLength);
+  });
+};
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const attemptFetch = (url: string) =>
+  new Promise((resolve, reject) => {
+    const fetchTodoPromise = fetch(url);
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+    fetchTodoPromise.then((response: Response) => {
+      if (response.ok) {
+        resolve(response.json());
+      } else {
+        reject(`Invalid Response: ${response.status}`);
+      }
+    });
+  });
+
+const fetchWithTimeout = (url: string, timeout: number) => {
+  return Promise.race([attemptFetch(url), makeTimeoutPromise(timeout)]);
+};
+
+const fetchWithTimeoutAndRetries = (
+  url: string,
+  timeout: number,
+  maxRetries: number
+) =>
+  new Promise((resolve, reject) => {
+    let attempts = 1;
+    const executeFetch = () => {
+      const newFetchAttempt = fetchWithTimeout(url, timeout);
+
+      newFetchAttempt
+        .then((data) => {
+          resolve({ data, attempts });
+        })
+        .catch((reason) => {
+          console.error(reason, attempts);
+          if (attempts < maxRetries) {
+            attempts++;
+            executeFetch();
+          } else {
+            reject("All retries failed!");
+          }
+        });
+    };
+
+    executeFetch();
+  });
+
+const result = fetchWithTimeoutAndRetries(
+  "https://jsonplaceholder.typicode.com/todos/1",
+  70,
+  5
+);
+
+result
+  .then((result) => console.log(result, "final promise!"))
+  .catch((reason) => console.log(reason));
